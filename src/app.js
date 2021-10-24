0af9715774f07eb9ef31295c548688b3eab0e043
Fatime Nur <fatimenurc@gmail.com>
2021-10-25 00:36:18 +0300

    first lesson 18 ekim

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("hey babe!!")
+
+var dolarDun = 9.20
+var dolarBugun = 9.30
+
+let konutKredileri=["konut","emlak","kamu"]
+let konutKredileri2=["konut","emlak","kamu",12,[1,2,3]]
+//console.log(konutKredileri)
+
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log(konutKredileri[index])
+}
